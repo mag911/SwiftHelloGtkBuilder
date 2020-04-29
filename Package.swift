9cd3b75750db262012e0b4aaa462a91c546a4df4
mag911 <mag@nilskor.com>
2020-04-29 10:22:14 +0800

    Updated package.swift to mag911

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "HelloGtkBuilder",
     dependencies: [
-        .package(url: "https://github.com/rhx/SwiftGtk.git", .branch("master")),
+        .package(url: "https://github.com/mag911/SwiftGtk.git", .branch("master")),
     ],
     targets: [
         .target(name: "HelloGtkBuilder", dependencies: ["Gtk"]),
